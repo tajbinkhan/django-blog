5de8343754a64a2260cea5717e0e27c50751f485
Evan Khan <tajbink@gmail.com>
2020-10-04 14:28:18 +0600

    v3.6(Image Resize Reduce)

--- blog/forms.py
+++ blog/forms.py
@@ -17,7 +17,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'overview', 'content', 'thumbnail', 'categories', 'previous_post', 'next_post')
+        fields = ('title', 'overview', 'content', 'img_thumbnail', 'categories', 'previous_post', 'next_post')
 
     def __init__(self, *args, **kwargs):
         super(PostForm,self).__init__(*args, **kwargs)
--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+from PIL import Image
 from .utils import unique_slug_generator
 from django.urls import reverse
 from django.shortcuts import redirect
@@ -19,8 +20,8 @@ def get_filename_ext(filepath):
 def upload_image_path(instance, filename):
     new_filename = datetime.datetime.now()
     name, ext = get_filename_ext(filename)
-    final_filename = f'blog-{new_filename}{ext}'
-    return f'media/{final_filename}'
+    final_filename = f'{new_filename}{ext}'
+    return f'blog-thumbnail/{final_filename}'
 
 class Category(models.Model):
     title = models.CharField(max_length=120)
@@ -46,7 +47,7 @@ class Post(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField(Category)
-    thumbnail = models.ImageField(upload_to=upload_image_path)
+    img_thumbnail = models.ImageField(upload_to=upload_image_path)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     previous_post = models.ForeignKey('self', related_name='previous', on_delete=models.SET_NULL, blank=True, null=True)
     next_post = models.ForeignKey('self', related_name='next', on_delete=models.SET_NULL, blank=True, null=True)
@@ -60,6 +61,14 @@ class Post(models.Model):
     def get_comments(self):
         return self.comments.all().order_by('-created_on')
 
+    def save(self):
+        super().save()
+
+        img = Image.open(self.img_thumbnail.path)
+        if img.height > 426 or img.width > 640:
+            output_size = (426, 640)
+            img.thumbnail(output_size)
+            img.save(self.img_thumbnail.path)
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
