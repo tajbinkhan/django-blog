2812de2911ea515bc59a3fcf0bc40797aac4505d
Tajbin Khan <tajbink@gmail.com>
2023-07-25 21:55:57 +0600

    Update apps.py

--- site_setting/apps.py
+++ site_setting/apps.py
@@ -2,8 +2,8 @@ from django.apps import AppConfig
 
 
 class SiteSettingConfig(AppConfig):
-	default_auto_field = 'django.db.models.BigAutoField'
-	name = 'site_setting'
+	default_auto_field = "django.db.models.BigAutoField"
+	name = "site_setting"
 	verbose_name = "Site Settings"
 
 	def ready(self):
