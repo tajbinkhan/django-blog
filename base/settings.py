0ae36bd300e55c40affca86e54f0d61a23b46fda
Tajbin Khan <tajbink@gmail.com>
2023-08-27 07:14:56 +0600

    Bug Fixed

--- base/settings.py
+++ base/settings.py
@@ -17,97 +17,107 @@ from .ckeditor_configs import CKEDITOR_CONFIGS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = BASE_DIR/ 'templates'
-STATIC_DIR = BASE_DIR/ 'static'
-STATIC_ROOT = BASE_DIR/ 'staticfiles'
-MEDIA_ROOT = BASE_DIR/ 'media'
+TEMPLATE_DIR = BASE_DIR / "templates"
+STATIC_DIR = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'evankhan.pythonanywhere.com']
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "evankhan.pythonanywhere.com"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-	'jazzmin',
-	'django.contrib.admin',
-	'django.contrib.auth',
-	'django.contrib.contenttypes',
-	'django.contrib.sessions',
-	'django.contrib.messages',
-	'django.contrib.staticfiles',
-
+	"jazzmin",
+	"django.contrib.admin",
+	"django.contrib.auth",
+	"django.contrib.contenttypes",
+	"django.contrib.sessions",
+	"django.contrib.messages",
+	"django.contrib.staticfiles",
 	# My Apps
-	'blog.apps.BlogConfig',
-	'user.apps.UserConfig',
-	'search.apps.SearchConfig',
-	'site_setting.apps.SiteSettingConfig',
-
+	"blog.apps.BlogConfig",
+	"user.apps.UserConfig",
+	"search.apps.SearchConfig",
+	"site_setting.apps.SiteSettingConfig",
 	# Installed Apps
-	'crispy_forms',
+	"crispy_forms",
 	"crispy_bootstrap5",
-	'allauth',
-	'allauth.account',
-	'allauth.socialaccount',
-	'allauth.socialaccount.providers.facebook',
-	'allauth.socialaccount.providers.google',
-	'ckeditor',
+	"allauth",
+	"allauth.account",
+	"allauth.socialaccount",
+	"allauth.socialaccount.providers.facebook",
+	"allauth.socialaccount.providers.google",
+	"ckeditor",
 ]
 
 MIDDLEWARE = [
-	'django.middleware.security.SecurityMiddleware',
-	'whitenoise.middleware.WhiteNoiseMiddleware',
-	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.common.CommonMiddleware',
-	'django.middleware.csrf.CsrfViewMiddleware',
-	'django.contrib.auth.middleware.AuthenticationMiddleware',
-	'django.contrib.messages.middleware.MessageMiddleware',
-	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	"django.middleware.security.SecurityMiddleware",
+	"whitenoise.middleware.WhiteNoiseMiddleware",
+	"django.contrib.sessions.middleware.SessionMiddleware",
+	"django.middleware.common.CommonMiddleware",
+	"django.middleware.csrf.CsrfViewMiddleware",
+	"django.contrib.auth.middleware.AuthenticationMiddleware",
+	"django.contrib.messages.middleware.MessageMiddleware",
+	"django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'base.urls'
+ROOT_URLCONF = "base.urls"
 
 TEMPLATES = [
 	{
-		'BACKEND': 'django.template.backends.django.DjangoTemplates',
-		'DIRS': [TEMPLATE_DIR],
-		'APP_DIRS': True,
-		'OPTIONS': {
-			'context_processors': [
-				'django.template.context_processors.debug',
-				'django.template.context_processors.request',
-				'django.contrib.auth.context_processors.auth',
-				'django.contrib.messages.context_processors.messages',
-				'site_setting.context_processors.sidebar',
+		"BACKEND": "django.template.backends.django.DjangoTemplates",
+		"DIRS": [TEMPLATE_DIR],
+		"APP_DIRS": True,
+		"OPTIONS": {
+			"context_processors": [
+				"django.template.context_processors.debug",
+				"django.template.context_processors.request",
+				"django.contrib.auth.context_processors.auth",
+				"django.contrib.messages.context_processors.messages",
+				"site_setting.context_processors.sidebar",
 			],
 		},
 	},
 ]
 
-WSGI_APPLICATION = 'base.wsgi.application'
+WSGI_APPLICATION = "base.wsgi.application"
 
 AUTHENTICATION_BACKENDS = [
-	'django.contrib.auth.backends.ModelBackend',
-	'allauth.account.auth_backends.AuthenticationBackend',
+	"django.contrib.auth.backends.ModelBackend",
+	"allauth.account.auth_backends.AuthenticationBackend",
 ]
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-	'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-		'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+	DATABASES = {
+		"default": {
+			"ENGINE": "django.db.backends.sqlite3",
+			"NAME": BASE_DIR / "db.sqlite3",
+		}
 	}
-}
+else:
+	DATABASES = {
+		"default": {
+			"ENGINE": "django.db.backends.postgresql",
+			"NAME": config("DATABASE_NAME"),
+			"USER": config("DATABASE_USER"),
+			"PASSWORD": config("DATABASE_PASSWORD"),
+			"HOST": config("DATABASE_HOST"),
+			"PORT": config("DATABASE_PORT"),
+		}
+	}
 
 
 # Password validation
@@ -115,16 +125,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
 	{
-		'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+		"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
 	},
 	{
-		'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+		"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
 	},
 	{
-		'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+		"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
 	},
 	{
-		'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+		"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
 	},
 ]
 
@@ -132,9 +142,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'Asia/Dhaka'
+TIME_ZONE = "Asia/Dhaka"
 
 USE_I18N = True
 
@@ -145,36 +155,36 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATICFILES_DIRS = [STATIC_DIR]
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
-MEDIA_URL = '/media/'
+MEDIA_URL = "/media/"
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
-LOGIN_REDIRECT_URL = 'blog-home'
-LOGIN_URL = 'account_login'
+LOGIN_REDIRECT_URL = "blog-home"
+LOGIN_URL = "account_login"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_PORT = config("EMAIL_PORT")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
 # Shortcodes
-ADMIN_PANEL = 'admin'
+ADMIN_PANEL = "admin"
 
 # Django All Auth Settings
 SITE_ID = 1
 ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION  = "mandatory"
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_EMAIL_SUBJECT_PREFIX = ""
 ACCOUNT_MAX_EMAIL_ADDRESSES = 2
 ACCOUNT_LOGIN_ON_EMAIL_CONFIRMATION = False
@@ -182,10 +192,10 @@ ACCOUNT_LOGOUT_REDIRECT_URL = "account_login"
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 ACCOUNT_SIGNUP_REDIRECT_URL = "account_login"
 SOCIALACCOUNT_EMAIL_VERIFICATION = False
-ACCOUNT_ADAPTER = 'user.adapter.CustomAccountAdapter'
+ACCOUNT_ADAPTER = "user.adapter.CustomAccountAdapter"
 ACCOUNT_FORMS = {
-	'login': 'user.forms.CustomLoginForm',
-	'signup': 'user.forms.CustomSignupForm',
+	"login": "user.forms.CustomLoginForm",
+	"signup": "user.forms.CustomSignupForm",
 }
 
 
