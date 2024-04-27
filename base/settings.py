defaefd0d389afd6c4e98f3671ff5d07af15c669
Tajbin Khan <tajbink@gmail.com>
2024-04-27 18:56:11 +0600

    Update ALLOWED_HOSTS in settings.py to include .onrender.com

--- base/settings.py
+++ base/settings.py
@@ -31,7 +31,13 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", ".vercel.app", ".pythonanywhere.com"]
+ALLOWED_HOSTS = [
+	"127.0.0.1",
+	"localhost",
+	".vercel.app",
+	".pythonanywhere.com",
+	".onrender.com",
+]
 
 
 # Application definition
